bab6e8030fe384135bd5d7cf249183e61826909a
Ramzy Kermad <kermad.r@sncf.fr>
2023-12-18 10:06:27 +0100

    Updated Repository to handle fetching data from Room db local storage

--- app/src/main/java/com/example/messagingapp/repositories/UsersRepository.kt
+++ app/src/main/java/com/example/messagingapp/repositories/UsersRepository.kt
@@ -1,13 +1,17 @@
 package com.example.messagingapp.repositories
 
+import com.example.messagingapp.db.AppDatabase
+import com.example.messagingapp.db.entities.CompleteUser
+import com.example.messagingapp.model.CompleteUserDto
 import com.example.messagingapp.model.user_model.UserData
 import com.example.messagingapp.network.services.UserApiService
+import com.example.messagingapp.utils.mapCompleteUserDataFromServer
 import io.reactivex.rxjava3.core.Flowable
 
 class UsersRepository(
-    private val userService: UserApiService
+    private val userService: UserApiService,
+    private val database: AppDatabase
 ) {
-
     fun getRandomListOfUsers(size: Int): Flowable<List<UserData>> {
         return userService.getOneRandomUserData(
             "fr_FR",
@@ -23,5 +27,18 @@ class UsersRepository(
         ).map {
             it.data
         }
+    }
+
+    fun getLocalUserFromRoomDb(): List<CompleteUser> {
+        return this.database.completeUserDao().getAll()
+    }
+
+    fun saveUsersToLocalStorage(users: List<CompleteUserDto>) {
+        this.database.completeUserDao()
+            .insertAll(
+                users.map {
+                    mapCompleteUserDataFromServer(it)
+                }
+            )
     }
 }
