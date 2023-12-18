1d4b27c1399e602f08f9fe1ae193ffd31de6f388
Ramzy Kermad <kermad.r@sncf.fr>
2023-12-18 10:03:55 +0100

    Added Complete User and Message db entities

--- app/src/main/java/com/example/messagingapp/db/entities/CompleteUser.kt
+++ app/src/main/java/com/example/messagingapp/db/entities/CompleteUser.kt
@@ -1,3 +1,19 @@
 package com.example.messagingapp.db.entities
 
-data class CompleteUser()
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.Relation
+
+@Entity(tableName = "complete_users")
+data class CompleteUser(
+    @PrimaryKey(autoGenerate = true) val uid: Int = 0,
+    @ColumnInfo(name = "user_id") val userId: String,
+    @ColumnInfo(name = "user_is_premium") val isUserPremium: Boolean,
+    @ColumnInfo(name = "user_name") val userName: String,
+    @ColumnInfo(name = "user_is_profile_pic_id") val profilePicId: Int,
+    @ColumnInfo(name = "user_last_name") val userLastName: String,
+    @ColumnInfo(name = "user_last_seen_date") val lastSeenDate: String,
+    @ColumnInfo(name = "user_phone_number") val phoneNumber: String,
+    @ColumnInfo(name = "user_status_description") val statusDescription: String,
+)
--- app/src/main/java/com/example/messagingapp/db/entities/Messages.kt
+++ app/src/main/java/com/example/messagingapp/db/entities/Messages.kt
@@ -1,3 +1,13 @@
 package com.example.messagingapp.db.entities
 
-data class Messages()
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "message")
+data class Messages(
+    @PrimaryKey(autoGenerate = true) val uid: Int = 0,
+    @ColumnInfo(name = "is_mine") val isMyMessage: Boolean,
+    @ColumnInfo(name = "user_id") val userid: String,
+    @ColumnInfo(name = "message_content") val content: String
+)
