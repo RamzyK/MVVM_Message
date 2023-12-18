1a5c8d4e374c063ff6dd21fef8e940f286dd99c1
Ramzy Kermad <kermad.r@sncf.fr>
2023-12-18 10:03:29 +0100

    Added complete user and message DAO interfaces to handle data accessing

--- app/src/main/java/com/example/messagingapp/db/daos/CompleteUserDao.kt
+++ app/src/main/java/com/example/messagingapp/db/daos/CompleteUserDao.kt
@@ -1,4 +1,22 @@
 package com.example.messagingapp.db.daos
 
-class CompleteUserDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.messagingapp.db.entities.CompleteUser
+
+@Dao
+interface CompleteUserDao {
+    @Query("SELECT * FROM complete_users")
+    fun getAll(): List<CompleteUser>
+
+    @Query("SELECT * FROM complete_users WHERE user_id = :userId ")
+    fun getUserById(userId: String): CompleteUser
+
+    @Insert
+    fun insertAll(users: List<CompleteUser>)
+
+    @Delete
+    fun delete(user: CompleteUser)
 }
--- app/src/main/java/com/example/messagingapp/db/daos/MessagesDao.kt
+++ app/src/main/java/com/example/messagingapp/db/daos/MessagesDao.kt
@@ -1,4 +1,12 @@
 package com.example.messagingapp.db.daos
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.messagingapp.db.entities.Messages
+
+@Dao
 interface MessagesDao {
+
 }
