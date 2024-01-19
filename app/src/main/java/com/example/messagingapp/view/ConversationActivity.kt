7232a4346eaeea537dc3900707aacd5ff8af5cdb
Ramzy Kermad <kermad.r@sncf.fr>
2024-01-19 12:01:27 +0100

    Removed unused import

--- app/src/main/java/com/example/messagingapp/view/ConversationActivity.kt
+++ app/src/main/java/com/example/messagingapp/view/ConversationActivity.kt
@@ -3,7 +3,6 @@ package com.example.messagingapp.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
